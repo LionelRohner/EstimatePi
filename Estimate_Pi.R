c05b4c8b1d63a2a050a74e9cdc20afc7dbd14bb8
LionelRohner <64872630+LionelRohner@users.noreply.github.com>
2021-06-03 16:22:18 +0200

    Update

--- Estimate_Pi.R
+++ Estimate_Pi.R
@@ -1,37 +1,24 @@
 
+# Library -----------------------------------------------------------------
+
+library(rbenchmark)
+
 # Methods -----------------------------------------------------------------
 
 
-generate_points_distance <- function(n){
-  
-  # # generate x,y coordinates
-  # x <- runif(n)
-  # y <- runif(n)
+generate_points <- function(n){
   
   # put them in a matrix
   XY <- matrix(runif(2*n), nrow = n, ncol = 2)
   
-  # calculate vector norm and test if its bigger than the radius (i.e. 1)
-  dist <- sqrt(XY[,1]^2 + XY[,2]^2) < 1
-  return(dist)
+  return(XY)
+
 }
 
-
-
-generate_points_distance_avg <- function(n){
-
-  iterations <- n %/% 10
-  orderMatrix <- (2*n)%/%iterations
+get_distance <- function(XY){
   
-  list()
-  for (i in iterations){
-    # put them in a matrix
-    XY <- matrix(runif(orderMatrix), nrow = n, ncol = 2)
-    
-    # calculate vector norm and test if its bigger than the radius (i.e. 1)
-    dist <- sqrt(XY[,1]^2 + XY[,2]^2) < 1
-  }
-  
+  # calculate vector norm and test if its bigger than the radius (i.e. 1)
+  dist <- sqrt(XY[,1]^2 + XY[,2]^2) < 1
   return(dist)
 }
 
@@ -41,29 +28,43 @@ approx_pi <- function(dist){
   return(4*sum(dist)/length(dist))
 }
 
+dist <- generate_points(100000)
+
+plot(dist[,1],dist[,2])
+
+plot_pi_ratio <- function(dist)
 
 # Main Function -----------------------------------------------------------
 
 estimate_pi <- function(n){
-  pis <- c()
-  for (i in 1:10){
-    dist <- generate_points_distance(n = n%/%10)
-    approxed_pi <- approx_pi(dist = dist)
-    pis <- c(pis,approxed_pi)
-  }
-  return(mean(pi))
+  dist <- get_distance(generate_points(n = n))
+  return(approx_pi(dist = dist))
 }
 
 
 estimate_pi_v2 <- function(n){
   pis <- c()
-  for (i in 1:10){
-    dist <- generate_points_distance(n = n%/%10)
+  for (i in 1:1000){
+    points <- generate_points_distance(n = n%/%1000)
+    dist <- get_distance(points)
     approxed_pi <- approx_pi(dist = dist)
     pis <- c(pis,approxed_pi)
   }
-  return(mean(pi))
+  return(mean(pis))
 }
 
 
-estimate_pi(10000000)
+# BenchMark ---------------------------------------------------------------
+
+estimate_pi(1000000)
+estimate_pi_v2(1000000)
+
+n = 1e8
+
+test <- benchmark("v1" = {estimate_pi(n)},
+                  "v2" = {estimate_pi_v2(n)},
+                  replications = 2)
+
+test$meanTime <- test$elapsed/test$replications
+test
+ 
